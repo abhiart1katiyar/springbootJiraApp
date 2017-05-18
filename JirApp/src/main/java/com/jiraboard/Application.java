4ed910a068836778f178321c3ef3fa71f7d21077
Abhishek katiyar <akkatiyar@ap.equinix.com>
2017-05-18 10:05:20 +0800

    Added dependencies entry in pom.xml

--- JirApp/src/main/java/com/jiraboard/Application.java
+++ JirApp/src/main/java/com/jiraboard/Application.java
@@ -1,13 +1,14 @@
 package com.jiraboard;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application 
 {
     public static void main( String[] args )
     {
+    	SpringApplication.run(Application.class, args);
         System.out.println( "Hello World! Jenkins Commit check." );
     }
 }
